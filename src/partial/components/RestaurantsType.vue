68c3dec3fa3a2f38c8d02fb0c3acf8377a128a27
Jero1304 <calogeroleto13@gmail.com>
2023-05-19 16:57:34 +0200

    add riquadro

--- src/partial/components/RestaurantsType.vue
+++ src/partial/components/RestaurantsType.vue
@@ -7,11 +7,57 @@
                 <button>Pizza</button>
                 <button>Pizza</button>
                 <button>Pizza</button>
+                <button>Pizza</button>
+                <button>Pizza</button>
+                <button>Pizza</button>
+                <button>Pizza</button>
+                <button>Pizza</button>
+                <button>Pizza</button>
+                <button>Pizza</button>
+                <button>Pizza</button>
+                <button>Pizza</button>
+                <button>Pizza</button>
+                <button>Pizza</button>
+                <button>Pizza</button>
+                
             </div>
         </div>
 
         <div class="restaurants col-11">
-            ristorante
+            <div class="container">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="https://picsum.photos/200/300" alt="">
+                        pizzeria
+                    </div>
+                    <div class="col-3">
+                        <img src="https://picsum.photos/200/300" alt="">
+                        pizzeria
+                    </div>
+                    <div class="col-3">
+                        <img src="https://picsum.photos/200/300" alt="">
+                        pizzeria
+                    </div>
+                    <div class="col-3">
+                        <img src="https://picsum.photos/200/300" alt="">
+                        pizzeria
+                    </div>
+                    <div class="col-3">
+                        <img src="https://picsum.photos/200/300" alt="">
+                        pizzeria
+                    </div>
+                    <div class="col-3">
+                        <img src="https://picsum.photos/200/300" alt="">
+                        pizzeria
+                    </div>
+                    <div class="col-3">
+                        <img src="https://picsum.photos/200/300" alt="">
+                        pizzeria
+                    </div>
+                    
+                    
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -29,10 +75,11 @@ export default {
     margin: 0;
     flex-wrap: nowrap;
     flex-shrink: 1;
+    height: auto;
 
     .aside {
         background-color: red;
-        min-height: 200px;
+        // min-height: 200px;
         margin: 0;
 
         .aside_type {
@@ -41,17 +88,32 @@ export default {
             height: 100%;
             display: flex;
             flex-direction: column;
-            button{
-                padding: 0;
-                margin:auto;
+
+            button {
+                padding: 20px;
+                margin: 10px 0;
             }
         }
     }
 
     .restaurants {
         background-color: blue;
-        min-height: 200px;
+        // min-height: 200px;
         margin: 0;
+        .col-3{
+            border: 1px solid red;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+            img{
+                width: 100px;
+                height: 100px;
+                border-radius: 999px;
+                margin: 20px 0;
+            }
+
+        }
     }
 }
 </style>
