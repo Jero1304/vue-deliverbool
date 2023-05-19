ecf1126967f5ca4c490dfe4c2c0fbcab9a67f6aa
Jero1304 <calogeroleto13@gmail.com>
2023-05-19 15:25:09 +0200

    ADD style

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap';
+import './style.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
