2592b7ea8f76aa406427df7d1a179d32b5838a28
longo2002 <lombardo.fra2002@gmail.com>
2023-05-20 09:37:39 +0200

    NavBar

--- src/partial/components/NavBar.vue
+++ src/partial/components/NavBar.vue
@@ -1,12 +1,26 @@
 <template>
-    <div class="nav-container">
-        <div class="container-md">
-            <div class="navbar">
-                <button v-for="(button, index) in buttons" :key="index" class="navbar-button"
+    <div class="nav-container d-flex align-items-center">
+        <div class="container-sm ">
+            <img src="" alt="logo">
+        </div>
+        <div class="container-md ">
+            <div class="navbar align-items-center justify-content-end">
+                <button v-if="!isMobile" v-for="(button, index) in buttons" :key="index" class="navbar-button"
                     :class="{ active: activeButton === index }" @mouseover="setActiveButton(index)"
                     @mouseleave="resetActiveButton">
                     {{ button.text }}
                 </button>
+                <button v-else class="navbar-button" :class="{ active: activeButton === index }"
+                    @mouseover="setActiveButton(index)" @mouseleave="resetActiveButton" @click="toggleDropdown">
+                    Menu
+                </button>
+                <ul v-show="isDropdownOpen" class="dropdown-menu">
+                    <li v-for="(button, index) in buttons" :key="index" class="dropdown-menu-item"
+                        :class="{ active: activeButton === index }" @mouseover="setActiveButton(index)"
+                        @mouseleave="resetActiveButton">
+                        {{ button.text }}
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
@@ -21,11 +35,18 @@ export default {
                 { text: 'RESTORANTI' },
                 { text: 'TIPOLOGIE' },
                 { text: 'PIATTI' },
-                { text: 'RISTORATORE' },
-                { text: 'IMPOSTAZIONI' },
             ],
-            activeButton: null
+            activeButton: null,
+            isMobile: false,
+            isDropdownOpen: false,
         };
+    },
+    created() {
+        window.addEventListener('resize', this.checkMobile);
+        this.checkMobile();
+    },
+    destroyed() {
+        window.removeEventListener('resize', this.checkMobile);
     },
     methods: {
         setActiveButton(index) {
@@ -33,6 +54,13 @@ export default {
         },
         resetActiveButton() {
             this.activeButton = null;
+        },
+        checkMobile() {
+            this.isMobile = window.innerWidth < 576;
+        },
+        toggleDropdown() {
+            this.isDropdownOpen = !this.isDropdownOpen;
+            console.log("click")
         }
     }
 };
@@ -43,6 +71,7 @@ export default {
 
 .navbar {
     display: flex;
+    flex-wrap: wrap;
     margin-top: 5px;
     justify-content: space-around;
 }
@@ -52,8 +81,8 @@ export default {
     font-size: 12px;
     font-weight: 900;
     padding: 5px 10px;
-    background-color: $yellow;
-    color: #555;
+    background-color: $orange;
+    color: #fff;
     border-radius: 20px;
     transition: all 0.3s ease;
     margin-right: 10px;
@@ -63,14 +92,33 @@ export default {
 }
 
 .navbar-button.active {
-    background-color: $orange;
-    color: #fff;
+    background-color: $azur;
     animation-name: bounce;
+    box-shadow: 0 0 10px rgb(2, 214, 196);
 }
 
 .nav-container {
     background-color: $yellow_light;
     margin-top: -5px;
+}
+
+.dropdown-menu {
+    display: none;
+    position: absolute;
+    background-color: #f0f0f0;
+    list-style-type: none;
+    padding: 0;
+    margin-top: 5px;
+}
+
+.dropdown-menu-item {
+    padding: 10px;
+    cursor: pointer;
+}
+
+.dropdown-menu-item.active {
+    background-color: #555;
+    color: #fff;
 }
 
 @keyframes bounce {
@@ -84,6 +132,19 @@ export default {
 
     100% {
         transform: translateX(0);
+    }
+}
+
+@media (max-width: 576) {
+    .navbar {
+        flex-wrap: nowrap;
+        overflow-x: auto;
+    }
+
+    .navbar-button {
+        margin-right: 0;
+        margin-bottom: 10px;
+        flex-shrink: 0;
     }
 }
 </style>
