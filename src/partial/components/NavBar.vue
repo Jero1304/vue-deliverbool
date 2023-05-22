524b06026162ab4b012f61d7efbe7c78ab7cdabb
longo2002 <lombardo.fra2002@gmail.com>
2023-05-22 12:25:58 +0200

    Bootstrap integration attempt

--- src/partial/components/NavBar.vue
+++ src/partial/components/NavBar.vue
@@ -1,81 +1,42 @@
 <template>
-    <div class="nav-container d-flex align-items-center">
-        <div class="container-sm">
-            <img src="logo.png" alt="logo" />
-        </div>
-        <div class="container-md">
-            <div class="navbar align-items-center justify-content-end">
-                <button v-if="!isMobile" v-for="(button, index) in buttons" :key="index" class="navbar-button"
-                    :class="{ active: activeButton === index }" @mouseover="setActiveButton(index)"
-                    @mouseleave="resetActiveButton">
-                    {{ button.text }}
+    <nav class="navbar navbar-expand-lg bg-body-tertiary nav-container">
+        <div class="container-fluid justify-content-between">
+            <div>
+                <a class="navbar-brand" href="#"><img src="logo.png" alt="logo"></a>
+            </div>
+            <div>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
                 </button>
-                <div v-else>
-                    <button class="navbar-button" :class="{ active: isDropdownOpen }" @click="toggleDropdown">
-                        Menu
-                    </button>
-                    <ul v-show="isMobile && isDropdownOpen" class="dropdown-menu">
-                        <li v-for="(button, index) in buttons" :key="index" class="dropdown-menu-item"
-                            :class="{ active: activeButton === index }" @click="selectDropdownItem(index)">
-                            {{ button.text }}
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ">
+                        <li class="nav-item navbar-button">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item navbar-button">
+                            <a class="nav-link" href="#">Features</a>
+                        </li>
+                        <li class="nav-item navbar-button">
+                            <a class="nav-link" href="#">Pricing</a>
+                        </li>
+                        <li class="nav-item navbar-button">
+                            <a class="nav-link disabled">Disabled</a>
                         </li>
                     </ul>
                 </div>
             </div>
         </div>
-    </div>
+    </nav>
 </template>
-  
+
 <script>
-export default {
-    data() {
-        return {
-            buttons: [
-                { text: 'HOME' },
-                { text: 'RESTAURANTI' },
-                { text: 'TIPOLOGIE' },
-                { text: 'PIATTI' },
-            ],
-            activeButton: null,
-            isMobile: false,
-            isDropdownOpen: false,
-        };
-    },
-    mounted() {
-        window.addEventListener('resize', this.checkMobile);
-        this.checkMobile();
-    },
-    beforeDestroy() {
-        window.removeEventListener('resize', this.checkMobile);
-    },
-    methods: {
-        setActiveButton(index) {
-            this.activeButton = index;
-        },
-        resetActiveButton() {
-            this.activeButton = null;
-        },
-        checkMobile() {
-            this.isMobile = window.innerWidth < 768;
-            if (!this.isMobile) {
-                this.isDropdownOpen = false;
-            }
-        },
-        toggleDropdown() {
-            this.isDropdownOpen = !this.isDropdownOpen;
-        },
-        selectDropdownItem(index) {
-            this.activeButton = index;
-            this.isDropdownOpen = false;
-        },
-    },
-};
 </script>
 
 <style lang="scss">
 @import '../src/scss/variables.scss';
 
-.navbar {
+.navbar_scope {
     display: flex;
     flex-wrap: wrap;
     margin-top: 5px;
@@ -154,4 +115,3 @@ export default {
     }
 }
 </style>
-  
