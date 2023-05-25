a43924e15884207c7875fb8082f36b13b986ed68
longo2002 <lombardo.fra2002@gmail.com>
2023-05-25 15:45:06 +0200

    Responsi Re-Work

--- src/partial/components/Footer.vue
+++ src/partial/components/Footer.vue
@@ -2,7 +2,7 @@
     <div class="footer">
         <div class="container">
             <div class="row">
-                <div class="col-md-4">
+                <div class="col-sm-4 d-flex flex-column justify-content-center">
                     <h3>Informazioni</h3>
                     <ul>
                         <li><a href="#">Chi siamo</a></li>
@@ -10,7 +10,7 @@
                         <li><a href="#">Privacy Policy</a></li>
                     </ul>
                 </div>
-                <div class="col-md-4">
+                <div class="col-sm-4 d-flex flex-column justify-content-center">
                     <h3>Servizio clienti</h3>
                     <ul>
                         <li><a href="#">Contatti</a></li>
@@ -18,18 +18,13 @@
                         <li><a href="#">Assistenza online</a></li>
                     </ul>
                 </div>
-                <div class="col-md-4">
+                <div class="col-sm-4 d-flex flex-column justify-content-center">
                     <h3>Social</h3>
                     <ul>
                         <li><a href="#"><i class="fab fa-facebook"></i> Facebook</a></li>
                         <li><a href="#"><i class="fab fa-twitter"></i> Twitter</a></li>
                         <li><a href="#"><i class="fab fa-instagram"></i> Instagram</a></li>
                     </ul>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col">
-                    <p>&copy; 2023 Nome del tuo sito. Tutti i diritti riservati.</p>
                 </div>
             </div>
         </div>
@@ -48,7 +43,7 @@ export default {
 .footer {
     background-color: #f2f2f2;
     padding: 20px 0;
-    background-color: $yellow;
+    background-color: $orange;
 }
 
 .footer h3 {
@@ -73,7 +68,6 @@ export default {
 
 .footer p {
     font-size: 14px;
-    color: #777;
 }
 </style>
   
