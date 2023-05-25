ba91d0bddee37d9c3804176dc87175f70d8bcb85
GraceGenuardi <graziagenuardi96@gmail.com>
2023-05-25 17:09:17 +0200

    EDIT+STYLE sezione ristorante

--- src/partial/pages/MenuUser.vue
+++ src/partial/pages/MenuUser.vue
@@ -1,10 +1,21 @@
 <template>
     <div class="hero">
         <div class="hero-container">
-            <div class="container bg-primary info">
-                <h2>Ristorante</h2>
-                <p>descrizione</p>
-                <p>orari</p>
+            <div class="container info">
+                <h2>{Ristorante}</h2>
+                <p>{Descrizione}Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores placeat et vel incidunt cum ex ullam quos neque doloribus a! Nemo quidem quaerat sequi magnam eum sapiente molestiae saepe cum.
+                Ullam nisi doloribus necessitatibus vero labore sequi vel quis dicta voluptas, fuga modi ipsam. Perferendis enim molestias, reiciendis, incidunt velit tempora deserunt, fugiat doloremque nisi tenetur ad impedit accusamus dolore.
+                Esse amet expedita laboriosam ipsam corporis, mollitia, perferendis eius atque enim necessitatibus saepe unde minus quae ex. Deserunt repellendus beatae incidunt et ipsam, quia perferendis atque nulla sed. Distinctio, dignissimos!
+                Culpa voluptates veniam animi, illum veritatis dicta, nihil quia numquam, eius doloremque obcaecati ab quae odio magni ipsam cupiditate iure omnis soluta. Natus, sapiente nesciunt aut assumenda architecto cum velit.</p>
+                <p>{Orari di apertura e chiusura}</p>
+
+                <div class="container-img">
+                <img src="https://cdn.pixabay.com/photo/2022/05/22/13/21/healthy-7213383_1280.jpg" alt="gourmet">
+                <img src="https://cdn.pixabay.com/photo/2022/03/04/00/47/wine-7046276_1280.jpg" alt="gourmet">
+                <img src="https://cdn.pixabay.com/photo/2020/06/08/16/49/pizza-5275191_1280.jpg" alt="pizza-gourmet">
+                </div>
+
+
             </div>
         </div>
     </div>
@@ -17,20 +28,20 @@
 
         <div class="container">
             <div class="row">
-                <div class="aside">
+                <!-- <div class="aside">
                     <div class="aside-plate">
 
                         <div @click="previousPageType" :disabled="currentPageType === 1"> sinistra</div>
 
                         <div class="aside-card" v-for="(plateType, index) in paginateType"
                             @click="typeSelection(index, plateType), currentTypeRest(), pagesResest()">
-                            <img src="../../../public/images/plate.webp" alt="">
+                            <img src="../../../public/images/plate.webp" alt="food">
                             <p>{{ plateType }}</p>
                         </div>
 
                         <div @click="nextPageType" :disabled="currentPageType === totalPagesType"> destra</div>
                     </div>
-                </div>
+                </div> -->
 
                 <div class="plates p-3">
                     <div class="row">
@@ -40,7 +51,7 @@
                         <div class="row col-10 justify-content-center">
                             <template v-for="(plate, index) in paginatePlates" :key="index">
                                 <div class="col-2" v-if="plate.type.includes(currentType)">
-                                    <img src="../../../public/images/cibo.webp" alt="">
+                                    <img src="../../../public/images/cibo.webp" alt="food">
                                     <p>{{ plate.name }}</p>
                                 </div>
                             </template>
@@ -282,10 +293,10 @@ export default {
             currentType: '',
 
             currentPageType: 1,
-            itemsPerPageType: 4,
+            itemsPerPageType: 6,
 
             currentPagePlate: 1,
-            itemsPerPagePlate: 5,
+            itemsPerPagePlate: 6,
         }
     },
     methods: {
@@ -363,12 +374,31 @@ export default {
 
 <style lang="scss" scoped>
 .hero {
+    background-image: url('../components/img/pattern-hero.jpg');
+    background-size: cover;
+    padding: 20px;
     .hero-container {
         height: 500px;
         width: 100%;
-        background-image: url('https://picsum.photos/200/300');
+        margin-bottom: 50px;
         background-repeat: no-repeat;
         background-size: cover;
+        padding: 20px;
+        
+    }
+
+    .info{
+        background-color: rgba(209, 228, 222, 0.579);
+        text-align: center;
+        padding-bottom: 50px;
+        padding: 20px;
+    }
+
+    .container-img img {
+        width: 200px;
+        height: 200px;
+        margin: 10px;
+        border-radius: 10px;
     }
 }
 
