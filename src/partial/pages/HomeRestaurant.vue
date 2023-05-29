d9776af878190b6bc8c8066cf3bc49699e15d7d3
Cinzia Pulvirenti <cinzia2092@hotmail.it>
2023-05-29 12:53:34 +0200

    feat fix card

--- src/partial/pages/HomeRestaurant.vue
+++ src/partial/pages/HomeRestaurant.vue
@@ -5,11 +5,45 @@
         <div class="container d-flex justify-content-center flex-wrap gap-5">
             <div class="card" style="width: 18rem;">
                 <div class="box-image">
-                    <img src="https://www.focus.it/images/2020/06/23/sushi-di-salmone_1020x680.jpg" class="card-img-top" alt="...">
+                    <img src="https://www.focus.it/images/2020/06/23/sushi-di-salmone_1020x680.jpg" class="card-img-top"
+                        alt="...">
                 </div>
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Ingredienti: quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p class="card-text text-overflow">Ingredienti: accusamus sequi minima! text to build on the card title and make up the bulk of
+                        the card's content.</p>
+                    <div class="d-flex justify-content-center gap-2 button-edit-delete">
+                        <a href="#" class="btn btn-primary">Modifica</a>
+                        <a href="#" class="btn btn-primary">Elimina</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card" style="width: 18rem;">
+                <div class="box-image">
+                    <img src="https://wips.plug.it/cips/paginegialle.it/magazine/cms/2022/03/carne-pregiata.jpg?w=744&h=418&a=c"
+                        class="card-img-top" alt="...">
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text text-overflow">Ingredienti: quick example text to build on the card title and make up the bulk of
+                        the card's content.</p>
+                    <div class="d-flex justify-content-center gap-2 button-edit-delete">
+                        <a href="#" class="btn btn-primary">Modifica</a>
+                        <a href="#" class="btn btn-primary">Elimina</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card" style="width: 18rem;">
+                <div class="box-image">
+                    <img src="https://www.bofrost.it/on/demandware.static/-/Sites-IT-master-catalog/default/dwa1565c42/images/5683_PATATINE_JULIENNE__R.jpg"
+                        class="card-img-top" alt="...">
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text text-overflow">Ingredienti: quick example text to build on the card title and make up the bulk of
+                        the card's content.</p>
                     <div class="d-flex justify-content-center gap-2">
                         <a href="#" class="btn btn-primary">Modifica</a>
                         <a href="#" class="btn btn-primary">Elimina</a>
@@ -19,11 +53,13 @@
 
             <div class="card" style="width: 18rem;">
                 <div class="box-image">
-                    <img src="https://wips.plug.it/cips/paginegialle.it/magazine/cms/2022/03/carne-pregiata.jpg?w=744&h=418&a=c" class="card-img-top" alt="...">
+                    <img src="https://www.focus.it/images/2020/06/23/sushi-di-salmone_1020x680.jpg" class="card-img-top"
+                        alt="...">
                 </div>
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Ingredienti: quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p class="card-text text-overflow">Ingredienti: quick example text to build on the card title and make up the bulk of
+                        the card's content.</p>
                     <div class="d-flex justify-content-center gap-2">
                         <a href="#" class="btn btn-primary">Modifica</a>
                         <a href="#" class="btn btn-primary">Elimina</a>
@@ -32,12 +68,12 @@
             </div>
 
             <div class="card" style="width: 18rem;">
-                <div class="box-image">
-                    <img src="https://www.bofrost.it/on/demandware.static/-/Sites-IT-master-catalog/default/dwa1565c42/images/5683_PATATINE_JULIENNE__R.jpg" class="card-img-top" alt="...">
-                </div>
+                <img src="https://www.focus.it/images/2020/06/23/sushi-di-salmone_1020x680.jpg" class="card-img-top"
+                    alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Ingredienti: quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p class="card-text text-overflow">Ingredienti: quick example text to build on the card title and make up the bulk of
+                        the card's content.</p>
                     <div class="d-flex justify-content-center gap-2">
                         <a href="#" class="btn btn-primary">Modifica</a>
                         <a href="#" class="btn btn-primary">Elimina</a>
@@ -46,12 +82,11 @@
             </div>
 
             <div class="card" style="width: 18rem;">
-                <div class="box-image">
-                    <img src="https://www.focus.it/images/2020/06/23/sushi-di-salmone_1020x680.jpg" class="card-img-top" alt="...">
-                </div>
+                <img src="https://www.focus.it/images/2020/06/23/sushi-di-salmone_1020x680.jpg" alt="">
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Ingredienti: quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p class="card-text text-overflow">Ingredienti: quick example text to build on the card title and make up the bulk of
+                        the card's content.</p>
                     <div class="d-flex justify-content-center gap-2">
                         <a href="#" class="btn btn-primary">Modifica</a>
                         <a href="#" class="btn btn-primary">Elimina</a>
@@ -60,10 +95,12 @@
             </div>
 
             <div class="card" style="width: 18rem;">
-                <img src="https://www.focus.it/images/2020/06/23/sushi-di-salmone_1020x680.jpg" class="card-img-top" alt="...">
+                <img src="https://www.bofrost.it/on/demandware.static/-/Sites-IT-master-catalog/default/dwa1565c42/images/5683_PATATINE_JULIENNE__R.jpg"
+                    class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Ingredienti: quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p class="card-text text-overflow">Ingredienti: quick example text to build on the card title and make up the bulk of
+                        the card's content.</p>
                     <div class="d-flex justify-content-center gap-2">
                         <a href="#" class="btn btn-primary">Modifica</a>
                         <a href="#" class="btn btn-primary">Elimina</a>
@@ -72,10 +109,12 @@
             </div>
 
             <div class="card" style="width: 18rem;">
-      
+                <img src="https://www.my-personaltrainer.it/2020/09/07/hamburger_900x760.jpeg" class="card-img-top"
+                    alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Ingredienti: quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p class="card-text text-overflow">Ingredienti: quick example text to build on the card title and make up the bulk of
+                        the card's content.</p>
                     <div class="d-flex justify-content-center gap-2">
                         <a href="#" class="btn btn-primary">Modifica</a>
                         <a href="#" class="btn btn-primary">Elimina</a>
@@ -84,10 +123,12 @@
             </div>
 
             <div class="card" style="width: 18rem;">
-                <img src="https://www.bofrost.it/on/demandware.static/-/Sites-IT-master-catalog/default/dwa1565c42/images/5683_PATATINE_JULIENNE__R.jpg" class="card-img-top" alt="...">
+                <img src="https://wips.plug.it/cips/paginegialle.it/magazine/cms/2022/03/carne-pregiata.jpg?w=744&h=418&a=c"
+                    class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Ingredienti: quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p class="card-text text-overflow">Ingredienti: quick example text to build on the card title and make up the bulk of
+                        the card's content.</p>
                     <div class="d-flex justify-content-center gap-2">
                         <a href="#" class="btn btn-primary">Modifica</a>
                         <a href="#" class="btn btn-primary">Elimina</a>
@@ -96,46 +137,18 @@
             </div>
 
             <div class="card" style="width: 18rem;">
-                <img src="https://www.my-personaltrainer.it/2020/09/07/hamburger_900x760.jpeg" class="card-img-top" alt="...">
+                <img src="https://www.giallozafferano.it/images/249-24919/Pizza-napoletana_650x433_wm.jpg"
+                    class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Ingredienti: quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p class="card-text text-overflow">Ingredienti: quick example text to build on the card title and make up the bulk of
+                        the card's content.</p>
                     <div class="d-flex justify-content-center gap-2">
                         <a href="#" class="btn btn-primary">Modifica</a>
                         <a href="#" class="btn btn-primary">Elimina</a>
                     </div>
                 </div>
             </div>
-
-            <div class="card" style="width: 18rem;">
-                <img src="https://wips.plug.it/cips/paginegialle.it/magazine/cms/2022/03/carne-pregiata.jpg?w=744&h=418&a=c" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Ingredienti: quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <div class="d-flex justify-content-center gap-2">
-                        <a href="#" class="btn btn-primary">Modifica</a>
-                        <a href="#" class="btn btn-primary">Elimina</a>
-                    </div>
-                </div>
-            </div>
-
-
-            <div class="card" style="width: 18rem;">
-                <img src="https://www.giallozafferano.it/images/249-24919/Pizza-napoletana_650x433_wm.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Ingredienti: quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <div class="d-flex justify-content-center gap-2">
-                        <a href="#" class="btn btn-primary">Modifica</a>
-                        <a href="#" class="btn btn-primary">Elimina</a>
-                    </div>
-                </div>
-            </div>
-
-
-
-
-
         </div>
     </div>
 </template>
@@ -144,23 +157,34 @@
 import Delivery from '../components/Delivery.vue';
 
 export default {
-    components:{
+    components: {
         Delivery,
     }
 }
 </script>
 
-<style scoped>
-.section_home_ristaurant {
+<style lang="scss" scoped>.section_home_ristaurant {
     background-color: rgb(255, 204, 29);
-}   
-.card {
-    background-color: rgb(255, 204, 29);
-    border: none;
 }
 
-.box-image {
+.card {
+    background-color: rgb(255, 204, 29);
+
+img {
     width: 18rem;
+    height: 12rem;
+}
+
+.text-overflow {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+}
+
+}
+
+.button-edit-delete {
+    margin-top: auto;
 }
 
 </style>
