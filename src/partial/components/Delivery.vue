5aa0f7b04125e0d3f22da842a56837f01df28be1
GraceGenuardi <graziagenuardi96@gmail.com>
2023-05-31 12:44:21 +0200

    Edit prova ennesima

--- src/partial/components/Delivery.vue
+++ src/partial/components/Delivery.vue
@@ -25,7 +25,7 @@
         <div class="card-body">
           <h5 class="card-title text-uppercase mt-4">Food street irresistibile!</h5>
           <p>Scopri una vasta selezione di cibi di strada provenienti da tutto il mondo, dai deliziosi panini ai gustosi
-            falafel. Goditi il sapore autentico e l'atmosfera unica dello street food comodamente a casa tua.</p>
+            falafel! Goditi il sapore autentico e l'atmosfera unica dello street food comodamente a casa tua.</p>
 
         </div>
       </div>
